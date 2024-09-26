a37dea11678825add538c70b007bd2f4406af174
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-09-25 21:32:24 -0300

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
-from Flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate
+from flask_cors import CORS
+from models import db, User
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///mibasededatos.db'
+db.init_app(app)
+migrate = Migrate(app, db)
+CORS(app)
 
-app.run(host='localhost')
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Hello, World!"})
+
+@app.route('/createUser', methods=['POST'])
+def create():
+    data = request.json
+    usuario = User()
+
+    usuario.nombre = data['nombre']
+    usuario.apellido = data['apellido']
+    usuario.email = data['email']
+    usuario.password = data['password']
+
+    db.session.add(usuario)
+    db.session.commit()
+
+    return{
+        "message": "User created successfully",
+        "data": usuario.serialize()
+    }, 201
+
+if __name__== "__main__":
+    app.run(host='localhost', port=5002, debug=True)
